29554a1314d2498331d10e8ac0d00e397fa59863
snowflake-trial <gaardyn@gmail.com>
2025-10-17 23:40:23 -0500

    Update streamlit_app.py
    added api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,8 @@ if ingredients_list:
         if ingredients_string:
             session.sql(my_insert_stmt).collect()
             st.success(name_on_order + ', your Smoothie is ordered!', icon="✅")
+
+
+# New section to display smoothiefroot nutrition information
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
